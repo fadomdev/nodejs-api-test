4346575902ec7d7ad204ce3decfda1c7a16f8cf6
Francisco <franciscoaquino19@gmail.com>
2025-09-10 01:54:58 -0300

    Add logger utility and integrate logging in IndexController; update dependencies

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,14 @@
 import express from 'express'
+import logger from '../utils/logger'
 
 class IndexController {
   public getHello(req: express.Request, res: express.Response) {
+    logger.info('Hello endpoint was called')
     res.send('Hello, World!')
   }
 
   public getGoodbye(req: express.Request, res: express.Response) {
+    logger.info('Goodbye endpoint was called')
     res.send('Goodbye, World!')
   }
 }
